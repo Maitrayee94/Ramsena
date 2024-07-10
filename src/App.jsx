e84ba8b0576d00e37f680b4185ec3990549979e4
ranjithjupaka <ranjithjupaka@gmail.com>
2024-07-10 23:06:28 +0530

    added stake page

--- src/App.jsx
+++ src/App.jsx
@@ -4,48 +4,19 @@ import HomePage from './Pages/HomePage'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Navigate } from 'react-router-dom';
 import StakePage from './Pages/StakePage';
-import { Toaster } from "react-hot-toast";
-import { defaultWagmiConfig } from '@web3modal/wagmi/react/config'
+import { Toaster } from 'react-hot-toast'
 import { WagmiProvider } from 'wagmi'
-import { bsc, bscTestnet } from 'wagmi/chains'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { config } from './config'
 
-import {
-  createWeb3Modal,
-  useWeb3Modal,
-  useWeb3ModalEvents,
-  useWeb3ModalState,
-  useWeb3ModalTheme,
-} from '@web3modal/wagmi/react'
+import { createWeb3Modal } from '@web3modal/wagmi/react'
 
 const queryClient = new QueryClient()
 
 const projectId = 'e4afaa996192e3d484b46d43a9f6f870' //aeecfbcaaf30576d781f3da13a186c26   // c86a583333393f73c3218e131a7d46fc
 
-const metadata = {
-  name: 'Ramsena Stake',
-  description:
-    'Ramsena offers a paradigm shift in the way individuals or business connect, & communicate in todays digital landscape, that leverage the power of blockchain to enhance security, privacy, and flexibility in communication.',
-  url: 'https://stake.voipfinance.com',
-  icons: ['https://voip-web.netlify.app/assets/icon1-DGHckJj4.png'],
-}
-
-const wagmiConfig = defaultWagmiConfig({
-  chains: [bsc, bscTestnet],
-  projectId,
-  metadata: {
-    name: 'Web3Modal React Example',
-    description: 'Web3Modal React Example',
-    url: '',
-    icons: [],
-  },
-  auth: {
-    email: false,
-  },
-})
-
 createWeb3Modal({
-  wagmiConfig,
+  wagmiConfig: config,
   projectId,
   themeMode: 'dark',
   themeVariables: {
@@ -57,7 +28,7 @@ createWeb3Modal({
 function App() {
   return (
     <>
-      <WagmiProvider config={wagmiConfig}>
+      <WagmiProvider config={config}>
         <QueryClientProvider client={queryClient}>
           <BrowserRouter>
             <Toaster />
